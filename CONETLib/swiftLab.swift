fe382cf6d1b5eb8f631b183c0467a1125158feed
CoNET Project <info@CoNETTech.ca>
2024-10-23 07:32:03 -0700

    Added test cases

---
+++ CONETLib/swiftLab.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  CONETLib
+//
+//  Created by peter on 2024-10-23.
+//
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- CONETLibTests/CONETLibTests.swift
+++ CONETLibTests/CONETLibTests.swift
@@ -6,12 +6,28 @@
 //
 
 import Testing
+import XCTest
 @testable import CONETLib
+
 
 struct CONETLibTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
+
+class CONETLibTestsClass: XCTestCase {
+    
+    var swiftyLib: SwiftyLib!
+
+    override func setUp() {
+        swiftyLib = SwiftyLib()
+    }
+
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
     }
 
 }
