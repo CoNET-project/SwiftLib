0bb5bc57cf858c9fe880e1ba671743d495cb3b21
CoNET Project <info@CoNETTech.ca>
2024-10-23 05:56:26 -0700

    Initial Commit

---
+++ CONETLib/CONETLib.h
@@ -0,0 +1,18 @@
+//
+//  CONETLib.h
+//  CONETLib
+//
+//  Created by peter on 2024-10-23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CONETLib.
+FOUNDATION_EXPORT double CONETLibVersionNumber;
+
+//! Project version string for CONETLib.
+FOUNDATION_EXPORT const unsigned char CONETLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CONETLib/PublicHeader.h>
+
+
---
+++ CONETLibTests/CONETLibTests.swift
@@ -0,0 +1,17 @@
+//
+//  CONETLibTests.swift
+//  CONETLibTests
+//
+//  Created by peter on 2024-10-23.
+//
+
+import Testing
+@testable import CONETLib
+
+struct CONETLibTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
