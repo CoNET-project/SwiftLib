20b9c3f0f9461a7bc891c4cf9e9f9e3ceea0061a
CoNET Project <info@CoNETTech.ca>
2024-10-23 06:17:34 -0700

    Initial project setup

---
+++ CONETLibsExamples/CONETLibsExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CONETLibsExamplesApp.swift
+//  CONETLibsExamples
+//
+//  Created by peter on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct CONETLibsExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CONETLibsExamplesTests/CONETLibsExamplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CONETLibsExamplesTests.swift
+//  CONETLibsExamplesTests
+//
+//  Created by peter on 2024-10-23.
+//
+
+import Testing
+@testable import CONETLibsExamples
+
+struct CONETLibsExamplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
